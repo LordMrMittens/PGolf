14673017147085d5cc9bfa1f14d758af6621dda1
Joaquin <74403807+LordMrMittens@users.noreply.github.com>
2023-07-10 17:34:52 +0100

    PCG levelgen init

---
+++ Assets/Scripts/LevelGeneration/LevelPiece.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelPiece : MonoBehaviour
+{
+    public List<Transform> sockets = new List<Transform>();
+    public LevelPiece parent;
+    
+    int getSocketTries = 10;
+
+    public void AddSockets()
+    {
+        foreach (Transform SocketTransform in this.transform)
+        {
+            if (SocketTransform.CompareTag("Socket"))
+            {
+                sockets.Add(SocketTransform);
+            }
+        }
+    }
+
+    public void RemoveInvalidSocket(Transform socketLocation)
+    {
+
+        List<Transform> socketsToRemove = new List<Transform>();
+
+        foreach (Transform socket in sockets)
+        {
+            if (socketLocation.position == socket.position)
+            {
+                socketsToRemove.Add(socket);
+                Debug.Log("Socket to be removed: " + socket.name);
+            }
+        }
+
+        foreach (Transform socketToRemove in socketsToRemove)
+        {
+            sockets.Remove(socketToRemove);
+            Debug.Log("Socket Removed: " + socketToRemove.name);
+        }
+    }
+
+    public Transform GetSpawnSocket()
+    {
+        int indexToReturn = GenerateRandomIndex();
+
+        if (indexToReturn != -1 && getSocketTries >0)
+        {
+
+            if (Physics.OverlapSphere(sockets[indexToReturn].position, 3).Length > 1)
+            {
+                getSocketTries--;
+                return GetSpawnSocket();
+            }
+            getSocketTries = 10;
+            return sockets[indexToReturn];
+        }
+        getSocketTries = 10;
+        return null;
+
+    }
+
+    private int GenerateRandomIndex()
+    {
+        return Random.Range(0, sockets.Count);
+    }
+}
---
+++ Assets/Scripts/LevelGeneration/ProcLevelGenerator.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProcLevelGenerator : MonoBehaviour
+{
+    [SerializeField] GameObject[] levelPieces;
+    [SerializeField] int numberOfPieces;
+    Transform originPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        originPoint = GameObject.Find("Origin").transform;
+        Transform currentSpawn = originPoint;
+        List<GameObject> piecesInLevel = new List<GameObject>();
+        for (int i = 0; i < numberOfPieces; i++)
+        {
+            if(currentSpawn == null){
+                return;
+            }
+            int pieceInt = Random.Range(0, levelPieces.Length);
+            GameObject levelPiece = Instantiate(levelPieces[pieceInt], currentSpawn.position, Quaternion.identity);
+            LevelPiece piece = levelPiece.AddComponent<LevelPiece>();
+            piece.AddSockets();
+            piecesInLevel.Add(levelPiece);
+            piece.parent = currentSpawn.GetComponentInParent<LevelPiece>();
+            if (!piece.parent)
+            {
+                currentSpawn = piece.GetSpawnSocket();
+                continue;
+            }
+            
+            Vector3 direction = levelPiece.transform.position - piece.parent.gameObject.transform.position;
+            levelPiece.transform.position += direction;
+            levelPiece.transform.parent = piece.parent.transform;
+
+            piece.RemoveInvalidSocket(currentSpawn);
+
+            currentSpawn = piece.GetSpawnSocket();
+            if(currentSpawn ==null){
+                
+            }
+
+            //verify it doesnt overlap with other pieces
+            //find the next socket
+            //verify it does not go backwards into the existing scene
+            //continue
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
